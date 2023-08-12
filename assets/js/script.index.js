e500b005026606856d6ee9dccd68a9e392062b3d
Cumpri <rafaelcumpri.senai564@gmail.com>
2023-08-12 15:07:18 -0300

    feat : hide password in console

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -1,10 +1,27 @@
-function verify() {
+function callverify() {
     let username = document.getElementById('username').value
-    let password = document.getElementById('password').value
-
-    if (username == "Daniel" && password == "123") {
-        document.getElementById('error').innerHTML = ''
-    } else {
-        document.getElementById('error').innerHTML = "Senha ou Usuário incorretos"
-    }
+    daniel.acessScreen(username)
 }
+
+class Users {
+    constructor(name, pwd) {
+        this.name = name
+        let _pwd = pwd
+
+        this.verify = (senhaDigitada) => {
+            return senhaDigitada === _pwd;
+        }
+
+        this.acessScreen = (user) => {
+            let password = document.getElementById('password').value
+            if (this.verify(password) && this.name == user) {
+                console.log('funfou')
+            } else {
+                console.log('funfou2')
+            }
+        }
+    }
+    
+}
+
+const daniel = new Users('Daniel', '123')
