43920147ffaa8d7988f1522ffa0c5e8e11321b9f
Cumpri <rafaelcumpri.senai564@gmail.com>
2023-09-04 18:15:06 -0300

    fix : days cont

--- assets/js/script.home.js
+++ assets/js/script.home.js
@@ -22,8 +22,8 @@ function funtionalday() {
     document.getElementById(todayid).style.scale = '1.25'
     document.getElementById(todayid).style.borderRadius = '10px'
     
-    for(let i = 0; i < 5; i++) {
-        if (i != today) {
+    for(let i = 0; i < 6; i++) {
+        if (i < today) {
             let dayid = `day${i}`
             document.getElementById(dayid).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="0.75rem" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z"/></svg>
                                                         <h4>${getdayname(i)}</h4>`
